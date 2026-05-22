8d5baf43fef0e9a1a9ef59d995c13d1b07f063dc
Daniel <daniel.pustjens@gmail.com>
2026-05-22 16:03:52 +0200

    Add pgadmin

--- src/apphost/AppHost.cs
+++ src/apphost/AppHost.cs
@@ -1,9 +1,13 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
-var postgres = builder.AddPostgres("postgres");
+var postgres = builder.AddPostgres("postgres")
+    .WithDataVolume()
+    .WithPgAdmin();
+
 var database = postgres.AddDatabase("noneed2askdb");
 
 builder.AddProject<Projects.NoNeed2Ask_Api>("backend")
-    .WithReference(database);
+    .WithReference(database)
+    .WaitFor(database);
 
 builder.Build().Run();
